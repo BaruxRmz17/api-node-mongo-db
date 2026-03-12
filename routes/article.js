b9fc7e9021b656b8471a8e0848ba5aba88540e7f
Baruc Ramirez <barux111706@gmail.com>
2026-03-12 09:56:22 -0600

    fix: remove submodule and add routes as normal folder

---
+++ routes/article.js
@@ -0,0 +1,9 @@
+'use strict'
+
+var express = require('express');
+var ArticleController =  require('../controllers/article');
+
+var router = express.Router();
+
+router.get('/test', ArticleController.test);
+module.exports = router;
