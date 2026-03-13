a79eaa80564d68920e254b30aef7654b6d115e97
Baruc Ramirez <barux111706@gmail.com>
2026-03-13 10:13:39 -0600

    actualizando la api

--- routes/article.js
+++ routes/article.js
@@ -1,10 +1,15 @@
-// article.js
-'use strict'
+'use strict';
 
 var express = require('express');
-var ArticleController =  require('../controllers/article');
+var ArticleController = require('../controllers/article');
+const { route } = require('../app');
 
 var router = express.Router();
 
 router.get('/test', ArticleController.test);
+router.post('/save', ArticleController.save);
+router.get('/articles', ArticleController.getArticles);
+router.get('/articles/:last', ArticleController.getArticlesByLimit);
+router.get('/article/:id', ArticleController.getArticle);
+
 module.exports = router;
